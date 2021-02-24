62968127e104743a80359e7c70d5d383345713b6
Juan Cruz <jc23729@gmail.com>
2021-02-24 04:01:44 +0000

    function subtotals

--- index.js
+++ index.js
@@ -48,3 +48,28 @@ function logAtMost10(n) {
     console.log(i);
   }
 }
+
+// Time Complexity: O(1)
+
+function onlyElementsAtEvenIndex(array) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (i % 2 === 0) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
+// Time Complexity: O(n)
+
+function subtotals(array) {
+  let subtotalArray = [];
+  for (let i = 0; i < array.length; i++) {
+    let subtotal = 0;
+    for (let j = 0; j <= i; j++) {
+      subtotal += array[j];
+    }
+    subtotalArray.push(subtotal);
+  }
+  return subtotalArray;
+}
